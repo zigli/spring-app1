88ccb9ebc40774e20963ecde70bdc9786351cf77
ilgiz <4ilgiz@gmail.com>
2020-03-07 23:17:17 +0300

    Dependency Injection Использование setter

--- src/main/java/cf/tilgiz/spring/MusicPlayer.java
+++ src/main/java/cf/tilgiz/spring/MusicPlayer.java
@@ -5,11 +5,31 @@ package cf.tilgiz.spring;
  */
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+    public MusicPlayer() {}
 
     public void setMusic(Music music) {
         this.music = music;
