45ee39074c2a0a24f234962f11b033feac70a506
ilgiz <4ilgiz@gmail.com>
2020-03-07 22:51:56 +0300

    Внедрение зависимостей. Dependency Injection (DI)

--- src/main/java/cf/tilgiz/spring/TestSpring.java
+++ src/main/java/cf/tilgiz/spring/TestSpring.java
@@ -6,9 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
